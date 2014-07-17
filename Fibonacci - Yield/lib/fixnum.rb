96157d25d3a523add3a5df094a119f9cc068cf65
jaspreet21anand <jaspreet21anand@gmail.com>
2014-07-17 19:19:28 +0530

    delete yield command before loop - makes code even shorter

--- Fibonacci - Yield/lib/fixnum.rb
+++ Fibonacci - Yield/lib/fixnum.rb
@@ -2,11 +2,10 @@ class Fixnum
 
   def self.fibonaccing_upto
     num, next_num = 1, 1
-    yield( "#{num} #{next_num}" )
-
-    while num + next_num < 1000
+    
+    while num  < 1000
+      yield(num)
       num, next_num = next_num, num + next_num
-      yield(next_num)
     end
 
   end
