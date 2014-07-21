96c3365e90015d9b5169234473ab24459076bdbf
jaspreet21anand <jaspreet21anand@gmail.com>
2014-07-21 15:45:35 +0530

    add power() method to Array class - Power_Array

---
+++ Power_Array/bin/main.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/array'
+
+a = Array.new ([1, 2, 3, 4])
+
+puts "a_to_the_power(4) = #{ a.power(4) }"
+
+puts "#{ a }"
---
+++ Power_Array/lib/array.rb
@@ -0,0 +1,7 @@
+class Array
+
+  def power(to_the_power)
+    map { |array_item| array_item**to_the_power }
+  end
+
+end
