93d861d1427f4072710834aa074ec89fb2425bb8
jaspreet21anand <jaspreet21anand@gmail.com>
2014-07-22 16:16:45 +0530

    change method name to group_by_odd_even
    break method into two parts

--- Hash_inject/bin/main.rb
+++ Hash_inject/bin/main.rb
@@ -1,5 +1,5 @@
 require_relative '../lib/array'
 
-a = ['hello', 'abc', 125, 1982, 'a', 22, '2255669', 'hhhhh', 'b', 34]
+a = ['hello', 'abc', 125, 1982, 'a', 22, '2255669', 'hhhhh', 'b', 34, [1,2,3,4,5], { a: 1, b: 2 }]
 
-puts a.hash_by_odd_eve
+puts a.group_by_odd_even
--- Hash_inject/lib/array.rb
+++ Hash_inject/lib/array.rb
@@ -1,17 +1,23 @@
+require_relative 'numeric'
+
 class Array
 
-  def hash_by_odd_eve
-    hash_variable = Hash.new { |hash, key| hash[key] = [] }
-
-    for i in 0..length - 1
-      hash_variable[self[i].to_s.length] << self[i]
-    end
-
-    hash_variable.inject({ "odd" => [], "eve" => [] }) do |temp, (key, value)|
-      key.odd? ? temp["odd"] << value : temp["eve"] << value
+  def group_by_odd_even
+    
+    hash_by_length.inject({ odd: [], even: [] }) do |temp, (key, value)|
+      key.odd? ? temp[:odd] << value : temp[:even] << value
       temp
     end
     
   end
   
+  def hash_by_length
+    hash_variable = Hash.new { |hash, key| hash[key] = [] }
+
+    for i in 0...length
+      hash_variable[self[i].length] << self[i]
+    end
+
+    hash_variable
+  end
 end
