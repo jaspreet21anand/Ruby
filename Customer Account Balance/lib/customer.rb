379e6366cba842938b591c247a4647bbfbb8319d
jaspreet21anand <jaspreet21anand@gmail.com>
2014-07-23 18:18:46 +0530

    resolve naming issue of method and class variable

--- Customer Account Balance/lib/customer.rb
+++ Customer Account Balance/lib/customer.rb
@@ -1,18 +1,18 @@
 class Customer
   
-  @@account_number = 25111236
+  @@no_of_customers = 0
   attr_reader :balance, :accout_no
   attr_accessor :name
-
+  
   def initialize(name)
     @name = name
     @balance = 1000
-    @account_no = (@@account_number += 1)
+    @account_no = (@@no_of_customers += 1)
   end
 
   def deposit(amount)
     if amount <= 0 
-      wrong_input_message
+      erroneous_input_message
     else 
       @balance += amount
     end
@@ -20,7 +20,7 @@ class Customer
 
   def withdraw(amount)
     if amount <= 0
-      wrong_input_message
+      erroneous_input_message
     elsif @balance <= amount
       puts "Insufficient Balance in #{ @name }'s account"
     else
@@ -32,8 +32,8 @@ class Customer
     "Name: #{ @name } Account no: #{ @account_no } Balance: #{ @balance }"
   end
 
-  def wrong_input_message
+  def erroneous_input_message
     puts "----error----Please enter a positive amount for #{ @name }'s transaction"
   end
-
+  private :erroneous_input_message
 end
