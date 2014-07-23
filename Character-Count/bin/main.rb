556dff508f26c214fb2640ca5454de4157456fd9
jaspreet21anand <jaspreet21anand@gmail.com>
2014-07-23 15:21:23 +0530

    Character-Count add method count_char_by_type to String class

---
+++ Character-Count/bin/main.rb
@@ -0,0 +1,4 @@
+require_relative '../lib/string'
+
+puts 'Enter the String'
+puts gets.count_characters_by_type
---
+++ Character-Count/lib/string.rb
@@ -0,0 +1,19 @@
+class String
+
+  def count_characters_by_type
+    count = Hash.new { |hash, key| hash[key] = 0 }
+    chars.each do |character|
+      case character
+      when 'a'..'z'
+        count[:small_char] += 1
+      when 'A'..'Z'
+        count[:caps_char] += 1
+      when '0'..'9'
+        count[:digits] += 1
+      else
+        count[:special_char] += 1
+      end
+    end
+    count
+  end
+end
