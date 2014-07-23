920a7cb0830318cdd7d720d2831669758230a425
jaspreet21anand <jaspreet21anand@gmail.com>
2014-07-23 19:31:36 +0530

    change instance_variable to symbols

--- Customer Account Balance/lib/customer.rb
+++ Customer Account Balance/lib/customer.rb
@@ -1,5 +1,5 @@
 class Customer
-  
+
   @@no_of_customers = 0
   attr_reader :balance, :accout_no
   attr_accessor :name
@@ -22,18 +22,18 @@ class Customer
     if amount <= 0
       erroneous_input_message
     elsif @balance <= amount
-      puts "Insufficient Balance in #{ @name }'s account"
+      puts "Insufficient Balance in #{ :name }'s account"
     else
       @balance -= amount
     end
   end
 
   def to_s
-    "Name: #{ @name } Account no: #{ @account_no } Balance: #{ @balance }"
+    "Name: #{ :name } Account no: #{ :account_no } Balance: #{ :balance }"
   end
 
   def erroneous_input_message
-    puts "----error----Please enter a positive amount for #{ @name }'s transaction"
+    puts "----error----Please enter a positive amount for #{ :name }'s transaction"
   end
   private :erroneous_input_message
 end
