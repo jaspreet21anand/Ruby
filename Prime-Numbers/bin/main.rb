be11eaea18646650cfa35702d9ece6ffdb34bbee
jaspreet21anand <jaspreet21anand@gmail.com>
2014-07-25 10:52:00 +0530

    use array instead of string as return value

--- Prime-Numbers/bin/main.rb
+++ Prime-Numbers/bin/main.rb
@@ -1,4 +1,4 @@
 require_relative '../lib/integer'
 
 num = 1
-puts num.prime_numbers_till(67)
+puts num.prime_numbers_till(67).join(' ')
--- Prime-Numbers/lib/integer.rb
+++ Prime-Numbers/lib/integer.rb
@@ -1,11 +1,11 @@
 require 'prime'
 class Integer
 
-  def prime_numbers_till(up_limit)
-    string_of_prime = ""
-    string_of_prime << "2 " if up_limit > 2
-    (1..up_limit).step(2) do |number|
-      string_of_prime << "#{ number } " if number.prime?
+  def prime_numbers_till(upper_limit)
+    string_of_prime = []
+    string_of_prime << 2 if upper_limit > 2
+    (1..upper_limit).step(2) do |number|
+      string_of_prime << number if number.prime?
     end
     string_of_prime
   end
