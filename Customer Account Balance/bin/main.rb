cb6dbfe9ef3a3790e2bea671c6ee32032ccba017
jaspreet21anand <jaspreet21anand@gmail.com>
2014-07-25 10:07:26 +0530

    fix the code - code is not working and reslove spacing issue

--- Customer Account Balance/bin/main.rb
+++ Customer Account Balance/bin/main.rb
@@ -4,7 +4,7 @@ jaspreet = Customer.new('Jaspreet Singh')
 lovish = Customer.new('Lovish Mehta')
 sawan = Customer.new('Sawan Gupta')
 
-[jaspreet, lovish, sawan].each{ |obj| puts "\n", obj }
+[jaspreet, lovish, sawan].each { |obj| puts "\n", obj }
 
 puts 'lovish.deposit(1000)'
 puts 'sawan.withdraw(2500)'
@@ -14,7 +14,7 @@ lovish.deposit(1000)
 sawan.withdraw(2500)
 jaspreet.deposit(-100)
 
-[jaspreet, lovish, sawan].each{ |obj| puts "\n", obj }
+[jaspreet, lovish, sawan].each { |obj| puts "\n", obj }
 
 puts 'jaspreet.withdraw(700)'
 puts 'sawan.deposit(2000)'
@@ -22,4 +22,4 @@ puts 'sawan.deposit(2000)'
 jaspreet.withdraw(700)
 sawan.deposit(2000)
 
-[jaspreet, lovish, sawan].each{ |obj| puts "\n", obj }
+[jaspreet, lovish, sawan].each { |obj| puts "\n", obj }
--- Customer Account Balance/lib/customer.rb
+++ Customer Account Balance/lib/customer.rb
@@ -1,9 +1,9 @@
 class Customer
 
   @@no_of_customers = 0
-  attr_reader :balance, :accout_no
+  attr_reader :balance, :account_no
   attr_accessor :name
-  
+
   def initialize(name)
     @name = name
     @balance = 1000
@@ -11,9 +11,9 @@ class Customer
   end
 
   def deposit(amount)
-    if amount <= 0 
+    if amount <= 0
       erroneous_input_message
-    else 
+    else
       @balance += amount
     end
   end
@@ -22,18 +22,18 @@ class Customer
     if amount <= 0
       erroneous_input_message
     elsif @balance <= amount
-      puts "Insufficient Balance in #{ :name }'s account"
+      puts "Insufficient Balance in #{ name }'s account"
     else
       @balance -= amount
     end
   end
 
   def to_s
-    "Name: #{ :name } Account no: #{ :account_no } Balance: #{ :balance }"
+    "Name: #{ name } Account no: #{ account_no } Balance: #{ balance }"
   end
 
   def erroneous_input_message
-    puts "----error----Please enter a positive amount for #{ :name }'s transaction"
+    puts "----error----Please enter a positive amount for #{ name }'s transaction"
   end
   private :erroneous_input_message
 end
