f43a2a0d12922d22bf33765d3b63fa8b47f8742d
jaspreet21anand <jaspreet21anand@gmail.com>
2014-07-30 17:42:33 +0530

    use each_char + change variable name to character_count + ranges to constant

--- Character-Count/lib/string.rb
+++ Character-Count/lib/string.rb
@@ -1,20 +1,24 @@
 class String
 
+  SMALL_CHAR = 'a'..'z'
+  CAPS_CHAR = 'A'..'Z'
+  DIGITS = '0'..'9'
+
   def count_characters_by_type
-    count = Hash.new { |hash, key| hash[key] = 0 }
+    character_count = Hash.new(0)
     chars.each do |character|
       case character
-      when 'a'..'z'
-        count[:small_char] += 1
-      when 'A'..'Z'
-        count[:caps_char] += 1
-      when '0'..'9'
-        count[:digits] += 1
+      when SMALL_CHAR
+        character_count[:small_char] += 1
+      when CAPS_CHAR
+        character_count[:caps_char] += 1
+      when DIGITS
+        character_count[:digits] += 1
       else
-        count[:special_char] += 1
+        character_count[:special_char] += 1
       end
     end
-    count
+    character_count
   end
 
 end
