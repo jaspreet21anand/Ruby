def2c11ee5721ca34348129826f865c39a63d35d
jaspreet21anand <jaspreet21anand@gmail.com>
2014-07-30 18:01:22 +0530

    change constant name + use each_char

--- Character-Count/lib/string.rb
+++ Character-Count/lib/string.rb
@@ -1,18 +1,18 @@
 class String
 
-  SMALL_CHAR = 'a'..'z'
-  CAPS_CHAR = 'A'..'Z'
-  DIGITS = '0'..'9'
+  SMALL_CHAR_RANGE = 'a'..'z'
+  CAPS_CHAR_RANGE = 'A'..'Z'
+  DIGITS_RANGE = '0'..'9'
 
   def count_characters_by_type
     character_count = Hash.new(0)
-    chars.each do |character|
+    each_char do |character|
       case character
-      when SMALL_CHAR
+      when SMALL_CHAR_RANGE
         character_count[:small_char] += 1
-      when CAPS_CHAR
+      when CAPS_CHAR_RANGE
         character_count[:caps_char] += 1
-      when DIGITS
+      when DIGITS_RANGE
         character_count[:digits] += 1
       else
         character_count[:special_char] += 1
