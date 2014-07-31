dadf97bab6a06a22b0bcf12c7079ba82aef55278
jaspreet21anand <jaspreet21anand@gmail.com>
2014-07-31 15:00:28 +0530

    store calculation in variables + indentation + use each instead of inject

--- Sum_time-time_class/lib/time.rb
+++ Sum_time-time_class/lib/time.rb
@@ -1,7 +1,8 @@
 class Time
-
   SECONDS_IN_A_DAY = 86400
+  SECONDS_IN_AN_HOUR = 3600
   REGEX = /^(([0-1]\d)|(2[0-3])|\d):(([0-5]\d)|\d):(([0-5]\d)|\d)\z/
+
   def self.validate?(time_string)
     !!(time_string =~ REGEX)
   end
@@ -9,7 +10,7 @@ class Time
   def sum(time)
     summed_sec = time.to_sec + self.to_sec
     summed_time = normalize_time(summed_sec)
-    "#{"#{ summed_time[0] } day & " if summed_time[0] != 0 }#{ summed_time[1] }:#{ summed_time[2] }:#{ summed_time[3] }"
+    "#{"#{ summed_time[:day] } day & " if summed_time[:day] != 0 }#{ summed_time[:hour] }:#{ summed_time[:min] }:#{ summed_time[:sec] }"
   end
 
   protected
@@ -18,12 +19,14 @@ class Time
   end
 
   def normalize_time(summed_sec)
-    summed_time = []
-    summed_time[0] = summed_sec / SECONDS_IN_A_DAY
-    summed_time[1] = (summed_sec % SECONDS_IN_A_DAY) / 3600
-    summed_time[2] = ((summed_sec % SECONDS_IN_A_DAY) % 3600) / 60
-    summed_time[3] = ((summed_sec % SECONDS_IN_A_DAY) / 3600) % 60
-  summed_time
+    summed_time = Hash.new
+    summed_time[:day] = summed_sec / SECONDS_IN_A_DAY
+    residue_sec_after_day = summed_sec % SECONDS_IN_A_DAY
+    summed_time[:hour] = (residue_sec_after_day) / SECONDS_IN_AN_HOUR
+    residue_sec_after_hour = residue_sec_after_day % SECONDS_IN_AN_HOUR
+    summed_time[:min] = (residue_sec_after_hour) / 60
+    summed_time[:sec] = (residue_sec_after_hour) % 60 % 60
+    summed_time
   end
 
 end
