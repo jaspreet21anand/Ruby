dddc03cf2ef6218125f19d392c1c1f228842bc64
jaspreet21anand <jaspreet21anand@gmail.com>
2014-07-31 18:43:15 +0530

    return enum object from reverse_iterate

--- Reverse_iterate/lib/array.rb
+++ Reverse_iterate/lib/array.rb
@@ -1,9 +1,12 @@
 class Array
 
-  def reverse_iterate(&block)
+  def reverse_iterate
+    reverse_array = []
     for i in 1..length
-      block.call(self[-i])
+      reverse_array[i-1] = self[-i]
+      yield (self[-i]) if block_given?
     end
+    reverse_array.to_enum
   end
 
 end
