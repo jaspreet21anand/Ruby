91fa2b53d1c02880cef84617d9abf0f13f085cf2
jaspreet21anand <jaspreet21anand@gmail.com>
2014-08-04 13:02:06 +0530

    change constant name, make two different method for capital char check and blank field

--- Name-raise/lib/name.rb
+++ Name-raise/lib/name.rb
@@ -4,23 +4,30 @@ require_relative 'first_char_not_capital_error'
 class Name
 
   BLANK_STRING_REGEX = /\S+/
-  UPPER_CASE_FORMAT = /^[A-Z].*/
+  FIRST_CHAR_UPPER_CASE_REGEX = /^[A-Z].*/
   attr_reader :first_name, :last_name
 
   def initialize(first_name, last_name)
-    valid?(first_name, 'First Name')
-    valid?(last_name, 'Last Name')
+    is_blank?(first_name, 'First Name')
+    is_blank?(last_name, 'Last Name')
+    is_first_char_capital?(first_name, 'First Name')
 
     @first_name = first_name
     @last_name = last_name
   end
 
-  def valid?(string, field_name)
+  private
+
+  def is_blank?(string, field_name)
     if string !~ BLANK_STRING_REGEX
       raise BlankStringError, "ERROR: #{ field_name } Field left blank"
-    elsif field_name != 'Last Name' && string !~ UPPER_CASE_FORMAT
+    end
+  end
+
+  def is_first_char_capital?(string, field_name)
+    if string !~ FIRST_CHAR_UPPER_CASE_REGEX
       raise FirstCharNotCapitalError, "ERROR: In #{ field_name }, first character must be Capital"
     end
   end
-  private :valid?
+  
 end
