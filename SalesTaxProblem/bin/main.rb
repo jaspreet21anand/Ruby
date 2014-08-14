e82ea5459a1bdee3e0f0784af6e30996a6c9614b
jaspreet <chiragmongia@Chirags-MacBook-Pro.local>
2014-08-14 18:13:53 +0530

    remove y_or_no statement

--- SalesTaxProblem/bin/main.rb
+++ SalesTaxProblem/bin/main.rb
@@ -5,7 +5,6 @@ my_cart = Cart.new('Jaspreet')
 begin
   my_cart.add_product
   print "want to add more products? #{ Product::YES_OR_NO.values.join('/') }"
-  y_or_n = 1
 end while Product::YES_OR_NO.key(gets.chomp)
 
 my_cart.print_bill
